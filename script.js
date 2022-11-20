a4f49a10c0dd3592d0f548d1aeff18c81f33c3a7
tusharnankani <tusharnankani3@gmail.com>
2022-11-20 21:40:41 +0530

    :wrench: config: make diameter custom

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 let body = document.getElementsByTagName('body')[0];
-let globalMapForCoordinates = {};
 
 let disappearAfter = 500; // trailing length
 let appearAfter = 40; // (inverse) number of dots
+let CIRCLE_DIAMETER = 1; // rem
 
 let joinLines = () => {
     let totalItems = document.querySelectorAll('div.item');
-    console.log(totalItems);
 }
 
 let createElement = (x = window.innerWidth / 2, y = window.innerHeight / 2) => {
-    const CIRCLE_DIAMETER = 0.5; /* rem */
 
     let newItem = document.createElement('div');
     newItem.classList.add('item');
 
     newItem.style.setProperty('top', `calc(${y}px - ${CIRCLE_DIAMETER / 2}rem`);
     newItem.style.setProperty('left', `calc(${x}px - ${CIRCLE_DIAMETER / 2}rem`);
+    newItem.style.setProperty('width', `${CIRCLE_DIAMETER}rem`);
+    newItem.style.setProperty('height', `${CIRCLE_DIAMETER}rem`);
 
     body.appendChild(newItem);
-    console.log(globalMapForCoordinates)
     setTimeout(() => body.removeChild(newItem), disappearAfter);
 }
 
