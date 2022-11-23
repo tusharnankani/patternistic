7609244cfcdbaf420f51fff0bb39c04690ef0d60
Tushar Nankani <tusharnankani3@gmail.com>
2022-11-24 01:45:48 +0530

    feat: add a control panel (#1)
    * :sparkles: feat: add a control panel
    
    * Restyle feat: add a control panel (#2)
    
    * Restyled by clang-format
    
    * Restyled by prettier
    
    * Restyled by whitespace
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
-let body = document.getElementsByTagName('body')[0];
+let main = document.getElementsByTagName("main")[0];
+let header = document.getElementsByTagName("header")[0];
 
-let disappearAfter = 500; // trailing length
-let appearAfter = 40; // (inverse) number of dots
-let CIRCLE_DIAMETER = 1; // rem
+let trailingLength = document.getElementById("trailingLength");
+let numberOfDots = document.getElementById("numberOfDots");
+let circleSize = document.getElementById("circleSize");
 
-let joinLines = () => {
-    let totalItems = document.querySelectorAll('div.item');
-}
+let disappearAfter = 300; // circles will disappear after n seconds —
+// propotional to trailing length
+let appearAfter = 50; // circles with appear after n seconds — inverse to number of dots
+let CIRCLE_DIAMETER = 1.5; // rem
+
+let setToDefault = () => {
+  disappearAfter = 300;
+  appearAfter = 50;
+  CIRCLE_DIAMETER = 1.5;
+};
 
 let createElement = (x = window.innerWidth / 2, y = window.innerHeight / 2) => {
+  let newItem = document.createElement("div");
+  newItem.classList.add("item");
 
-    let newItem = document.createElement('div');
-    newItem.classList.add('item');
+  newItem.style.setProperty("top", `calc(${y}px - ${CIRCLE_DIAMETER / 2}rem`);
+  newItem.style.setProperty("left", `calc(${x}px - ${CIRCLE_DIAMETER / 2}rem`);
+  newItem.style.setProperty("width", `${CIRCLE_DIAMETER}rem`);
+  newItem.style.setProperty("height", `${CIRCLE_DIAMETER}rem`);
 
-    newItem.style.setProperty('top', `calc(${y}px - ${CIRCLE_DIAMETER / 2}rem`);
-    newItem.style.setProperty('left', `calc(${x}px - ${CIRCLE_DIAMETER / 2}rem`);
-    newItem.style.setProperty('width', `${CIRCLE_DIAMETER}rem`);
-    newItem.style.setProperty('height', `${CIRCLE_DIAMETER}rem`);
-
-    body.appendChild(newItem);
-    setTimeout(() => body.removeChild(newItem), disappearAfter);
-}
+  main.appendChild(newItem);
+  setTimeout(() => main.removeChild(newItem), disappearAfter);
+};
 
 let handleMouseMove = (e) => {
-    createElement(e.x, e.y);
-    joinLines();
-    removeListener();
-}
+  createElement(e.x, e.y);
+  removeListener();
+};
 
 let removeListener = () => {
-    body.removeEventListener('mousemove', handleMouseMove)
-    setTimeout(() => addListener(), appearAfter)
-}
+  main.removeEventListener("mousemove", handleMouseMove);
+  setTimeout(() => addListener(), appearAfter);
+};
 
 let addListener = () => {
-    body.addEventListener('mousemove', handleMouseMove);
-}
+  main.addEventListener("mousemove", handleMouseMove);
+};
 
+setToDefault();
 addListener();
+
+header.addEventListener("mouseover", () => removeListener());
+
+circleSize.addEventListener("change", () => {
+  CIRCLE_DIAMETER = circleSize.value / 2;
+});
+
+trailingLength.addEventListener("change", () => {
+  disappearAfter = trailingLength.value;
+});
+
+numberOfDots.addEventListener("change", () => {
+  appearAfter = numberOfDots.max - numberOfDots.value;
+});
